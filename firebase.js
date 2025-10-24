7cf0882f394d840a45bac725a30c4e10958ccc0d
Diego Terranova <90009963+DTrra@users.noreply.github.com>
2025-10-24 19:54:07 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,14 +2,23 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyAVoifybADa59HT30qRvcdkeNVWuDx9cIA",
+  authDomain: "menulatronera.firebaseapp.com",
+  projectId: "menulatronera",
+  storageBucket: "menulatronera.firebasestorage.app",
+  messagingSenderId: "463087743255",
+  appId: "1:463087743255:web:0a663d75f50ca00dcc8752"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
