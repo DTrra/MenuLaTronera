e72d364509180ee4e0f611e06eaac8c0a2a82ac7
Diego Terranova <90009963+DTrra@users.noreply.github.com>
2025-10-24 19:55:02 -0300

    Remove commented Firebase configuration code

--- firebase.js
+++ firebase.js
@@ -2,20 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAVoifybADa59HT30qRvcdkeNVWuDx9cIA",
-  authDomain: "menulatronera.firebaseapp.com",
-  projectId: "menulatronera",
-  storageBucket: "menulatronera.firebasestorage.app",
-  messagingSenderId: "463087743255",
-  appId: "1:463087743255:web:0a663d75f50ca00dcc8752"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
